d41b29723c8f28b51a3bea3da155342eb5775cb2
wangc <blankeeee@gmail.com>
2016-03-08 19:32:52 +0800

    增加rxAdapter,编写 tag展示界面

--- app/src/androidTest/java/com/blanke/solebook/CurstomImageviewTest.java
+++ app/src/androidTest/java/com/blanke/solebook/CurstomImageviewTest.java
@@ -1,6 +1,8 @@
 package com.blanke.solebook;
 
 import android.app.Instrumentation;
+import android.os.Handler;
+import android.os.Looper;
 import android.test.InstrumentationTestCase;
 import android.view.View;
 import android.widget.Toast;
@@ -14,7 +16,7 @@ import com.socks.library.KLog;
 public class CurstomImageviewTest extends InstrumentationTestCase {
     private Instrumentation instrumentation;
     private TestCurstomImageviewActivity testActitity;
-
+    int i=0;
     @Override
     public void setUp() throws Exception {
         super.setUp();
@@ -23,13 +25,14 @@ public class CurstomImageviewTest extends InstrumentationTestCase {
 
     public void testName() throws Exception {
         testActitity = launchActivity("com.blanke.solebook", TestCurstomImageviewActivity.class, null);
+//        new Handler().postAtFrontOfQueue()
         Thread.sleep(10000);
     }
 
     @Override
     public void tearDown() throws Exception {
         super.tearDown();
-        KLog.d("activity  test end");
+        KLog.d("activity  test end   什么鬼shenmegui");
         Thread.sleep(10000);
     }
 }
--- app/src/main/java/com/blanke/solebook/core/taglist/TagListFragment.java
+++ app/src/main/java/com/blanke/solebook/core/taglist/TagListFragment.java
@@ -1,13 +1,16 @@
 package com.blanke.solebook.core.taglist;
 
 import android.content.res.Configuration;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.blanke.solebook.R;
 import com.blanke.solebook.base.BaseColumnFragment;
@@ -18,6 +21,8 @@ import com.blanke.solebook.core.taglist.persenter.TagListPersenter;
 import com.blanke.solebook.core.taglist.persenter.TagListPersenterImpl;
 import com.blanke.solebook.core.taglist.view.TagListView;
 import com.blanke.solebook.utils.SnackUtils;
+import com.minimize.android.rxrecycleradapter.RxDataSource;
+import com.minimize.android.rxrecycleradapter.SimpleViewHolder;
 import com.neu.refresh.NeuSwipeRefreshLayout;
 import com.neu.refresh.NeuSwipeRefreshLayoutDirection;
 import com.socks.library.KLog;
@@ -31,63 +36,20 @@ import java.util.List;
 
 import cn.iwgang.familiarrecyclerview.FamiliarRecyclerView;
 import jp.wasabeef.recyclerview.animators.SlideInUpAnimator;
+import rx.functions.Action1;
 
 /**
  * Tag  fragment
  * Created by Blanke on 16-2-22.
  */
 @EFragment(R.layout.fragment_tag_item)
-public class TagListFragment extends BaseColumnFragment<SwipeRefreshLayout, List<Tag>, TagListView, TagListPersenter>
-        implements TagListView, NeuSwipeRefreshLayout.OnRefreshListener {
-    private static final long LAZY_DELAY_TIME = Constants.LAZY_DELAY_TIME;
-    @ViewById(R.id.fragment_columnitem_recyclerview)
+public class TagListFragment extends BaseColumnFragment<LinearLayout, List<Tag>, TagListView, TagListPersenter>
+        implements TagListView {
+    @ViewById(R.id.fragment_tag_recyclerview)
     FamiliarRecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
 
-    @ViewById(R.id.contentView)
-    NeuSwipeRefreshLayout mSwipeRefreshLayout;
-    private TagItemAdapter mAdapter;
-
-    private int currentPage = 0;
-    private int PAGE_COUNT = Constants.PAGE_COUNT;
-
-    private boolean isCreateView = false, isVisible = false, isNetworkFinish = false;
-
-    public String getTitle() {
-        return mCurrentTagColumn.getName();
-    }
-
-    public static TagListFragment newInstance(TagColumn TagColumn) {
-        TagListFragment fragment = new TagListFragment_();
-        Bundle bundle = new Bundle();
-        bundle.putParcelable(ARG_TagCOLUMN, TagColumn);
-        fragment.setArguments(bundle);
-        return fragment;
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        KLog.d(mCurrentTagColumn.getName() + hashCode());
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        KLog.d(mCurrentTagColumn.getName() + hashCode());
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        KLog.d(mCurrentTagColumn.getName());
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        KLog.d(mCurrentTagColumn.getName() + hashCode());
-    }
-
+    private List<Tag> datas;
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
@@ -97,53 +59,20 @@ public class TagListFragment extends BaseColumnFragment<SwipeRefreshLayout, List
 
     @AfterViews
     void init() {
-        mAdapter = new TagItemAdapter(getActivity());
-        mRecyclerView.setAdapter(mAdapter);
-        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
-        mSwipeRefreshLayout.setOnRefreshListener(this);
-//        KLog.d(isNetworkFinish);
-        isCreateView = true;
-        lazyLoad();
+        RxDataSource<Tag> rxDataSource=new RxDataSource<>(datas);
+        rxDataSource.bindRecyclerView(mRecyclerView,R.layout.item_recyclerview_tag)
+                .subscribe(new Action1<SimpleViewHolder<Tag, ViewDataBinding>>() {
+                    @Override
+                    public void call(SimpleViewHolder<Tag, ViewDataBinding> holder) {
+                        ItemLayoutBinding b = viewHolder.getViewDataBinding();
+                    }
+                });
         mRecyclerView.setOnItemClickListener(new FamiliarRecyclerView.OnItemClickListener() {
             @Override
             public void onItemClick(FamiliarRecyclerView familiarRecyclerView, View view, int position) {
-                Tag Tag = mAdapter.getTags().get(position);
-//                DetailsActivity_.intent(getActivity()).Tag(Tag).start();
-                DetailsActivity_.start(getActivity(),
-                        (ImageView) view.findViewById(R.id.item_Tag_image), Tag);
             }
         });
         mRecyclerView.setItemAnimator(new SlideInUpAnimator());
-
-
-    }
-
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        isVisible = getUserVisibleHint();
-        lazyLoad();
-    }
-
-    private void lazyLoad() {//fragment懒加载，可见才网络加载
-        if (isCreateView && isVisible && !isNetworkFinish) {
-            mSwipeRefreshLayout.postDelayed(() -> mSwipeRefreshLayout.autoRefresh(), LAZY_DELAY_TIME);
-            isNetworkFinish = true;
-        }
-        if (isVisible && isCreateView && fab != null) {
-            fab.setOnClickListener(v -> mRecyclerView.smoothScrollToPosition(0));
-            fab.attachToRecyclerView(mRecyclerView);
-        }
-    }
-
-//    @Override
-//    public LceViewState<List<Tag>, TagListView> createViewState() {
-//        return new CastedArrayListLceViewState<>();
-//    }
-
-    //    @Override
-    public List<Tag> getData() {
-        return mAdapter.getTags() == null ? null : new ArrayList<>(mAdapter.getTags());
     }
 
     @Override
@@ -158,38 +87,17 @@ public class TagListFragment extends BaseColumnFragment<SwipeRefreshLayout, List
 
     @Override
     public void setData(List<Tag> data) {
-        KLog.d(mCurrentTagColumn.getName());
-        mSwipeRefreshLayout.setRefreshing(false);
-        if (data == null || data.size() == 0) {
-            return;
-        }
-        if (currentPage == 0) {
-            mAdapter.setData(data);
-            mRecyclerView.smoothScrollToPosition(0);
-        } else {
-            mAdapter.addData(data);
-        }
-        currentPage++;
+        datas=data;
     }
 
     @Override
     protected void showLightError(String msg) {
-        mSwipeRefreshLayout.setRefreshing(false);
         SnackUtils.show(mRecyclerView, msg);
     }
 
     @Override
     public void loadData(boolean pullToRefresh) {
-        getPresenter().getTagData(mCurrentTagColumn, pullToRefresh, PAGE_COUNT * currentPage, PAGE_COUNT);
+//        getPresenter().getTagData(mCurrentTagColumn, pullToRefresh, PAGE_COUNT * currentPage, PAGE_COUNT);
     }
 
-    @Override
-    public void onRefresh(NeuSwipeRefreshLayoutDirection direction) {
-        if (direction == NeuSwipeRefreshLayoutDirection.TOP) {
-            currentPage = 0;
-            loadData(true);
-        } else if (direction == NeuSwipeRefreshLayoutDirection.BOTTOM) {
-            loadData(true);
-        }
-    }
 }
