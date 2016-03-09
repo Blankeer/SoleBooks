f21684937a47b3ebf720d92d028d006e0224d387
wangc <blankeeee@gmail.com>
2016-03-09 17:52:18 +0800

    增加 tag type

--- app/src/main/java/com/blanke/solebook/manager/ColumnManager.java
+++ app/src/main/java/com/blanke/solebook/manager/ColumnManager.java
@@ -4,6 +4,8 @@ import com.blanke.solebook.base.BaseColumnFragment;
 import com.blanke.solebook.bean.BookColumn;
 import com.blanke.solebook.constants.Constants;
 import com.blanke.solebook.core.booklist.BookListFragment;
+import com.blanke.solebook.core.taglist.TagListFragment_;
+import com.socks.library.KLog;
 
 /**
  * Created by Blanke on 16-2-23.
@@ -14,6 +16,8 @@ public class ColumnManager {
         switch (bookColumn.getType()) {
             case Constants.TYPE_COLUMN_BOOK:
                 return BookListFragment.newInstance(bookColumn);
+            case Constants.TYPE_COLUMN_TAG:
+                return TagListFragment_.builder().build();
         }
         return BookListFragment.newInstance(bookColumn);
     }
