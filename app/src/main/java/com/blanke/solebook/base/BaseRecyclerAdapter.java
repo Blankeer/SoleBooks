a467cb0ae417bcf1329b587d1eaec0eea7a34c3e
wangc <blankeeee@gmail.com>
2016-03-09 17:50:57 +0800

    封装baseadapter

---
+++ app/src/main/java/com/blanke/solebook/base/BaseRecyclerAdapter.java
@@ -0,0 +1,40 @@
+package com.blanke.solebook.base;
+
+import android.content.Context;
+
+import com.joanzapata.android.recyclerview.QuickAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Blanke on 16-3-9.
+ */
+public abstract class BaseRecyclerAdapter<T> extends QuickAdapter<T> {
+
+
+    public BaseRecyclerAdapter(Context context, int layoutResId) {
+        super(context, layoutResId);
+    }
+
+    public BaseRecyclerAdapter(Context context, int layoutResId, List<T> data) {
+        super(context, layoutResId, data);
+    }
+
+    public void addAll(List<T> data) {
+        int temp = getItemCount();
+        mItems.addAll(data);
+        notifyItemRangeInserted(temp, data.size());
+    }
+
+    @Override
+    public void addItem(T item) {
+        mItems.add(item);
+        notifyItemInserted(getItemCount() - 1);
+    }
+
+    @Override
+    public void clear() {
+        mItems.clear();
+        notifyItemRangeRemoved(0, getItemCount());
+    }
+}
