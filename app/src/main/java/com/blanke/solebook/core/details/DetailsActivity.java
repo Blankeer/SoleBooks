71abdadb2279a98b1b96287a6fb6e55a1fa0137a
blanke-x11 <blankeeee@gmail.com>
2016-04-07 22:33:14 +0800

    修改share页面,测试ok

--- app/src/main/java/com/blanke/solebook/core/details/DetailsActivity.java
+++ app/src/main/java/com/blanke/solebook/core/details/DetailsActivity.java
@@ -12,7 +12,10 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -30,6 +33,7 @@ import com.blanke.solebook.core.details.view.DetailsView;
 import com.blanke.solebook.utils.AnimUtils;
 import com.blanke.solebook.utils.BitmapUtils;
 import com.blanke.solebook.utils.DialogUtils;
+import com.blanke.solebook.utils.ResUtils;
 import com.blanke.solebook.utils.SkinUtils;
 import com.blanke.solebook.utils.StatusBarCompat;
 import com.like.LikeButton;
@@ -50,7 +54,8 @@ import org.androidannotations.annotations.ViewById;
 import org.simple.eventbus.EventBus;
 import org.simple.eventbus.Subscriber;
 
-import cn.sharesdk.framework.ShareSDK;
+import java.lang.reflect.Method;
+
 import cn.sharesdk.onekeyshare.OnekeyShare;
 
 
@@ -189,6 +194,10 @@ public class DetailsActivity extends BaseSwipeBackActivity implements DetailsVie
 
         mPersenter = new DetailsPersenterImpl(this, book);
         mPersenter.initLikeState();
+        toolbar.setOnMenuItemClickListener(item -> {
+            showShare();
+            return false;
+        });
     }
 
     private void initThemeImg() {
@@ -256,32 +265,50 @@ public class DetailsActivity extends BaseSwipeBackActivity implements DetailsVie
         EventBus.getDefault().unregister(this);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.share, menu);
+        return true;
+    }
+
+    @Override
+    protected boolean onPrepareOptionsPanel(View view, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass() == MenuBuilder.class) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
+        }
+        return super.onPrepareOptionsPanel(view, menu);
+    }
+
     private void showShare() {
-        ShareSDK.initSDK(this);
+//        ShareSDK.initSDK(this);
+        String url = ResUtils.getResString(this, R.string.app_url);
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
         oks.disableSSOWhenAuthorize();
 
-// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle(getString(R.string.share));
+        oks.setTitle(book.getTitle());
         // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl(url);
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        oks.setText(book.getIntroContent().substring(0, 100) + "......");
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
+        oks.setUrl(url);
+        oks.setImageUrl(book.getImgL());
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
+        oks.setComment("");
         // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
+        oks.setSite(book.getTitle());
         // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
-
-// 启动分享GUI
+        oks.setSiteUrl(url);
         oks.show(this);
     }
 }
