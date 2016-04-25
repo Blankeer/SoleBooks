e4bf160531e3993e40f3454e833a05a5f93b5b31
blanke-x11 <blankeeee@gmail.com>
2016-04-25 22:49:40 +0800

    修改设置页面的滑动返回事件,和点击home图标finish()

--- app/src/main/java/com/blanke/solebook/core/settings/SettingsActivity.java
+++ app/src/main/java/com/blanke/solebook/core/settings/SettingsActivity.java
@@ -1,10 +1,11 @@
 package com.blanke.solebook.core.settings;
 
 
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.blanke.solebook.R;
+import com.blanke.solebook.base.BaseSwipeBackActivity;
 import com.blanke.solebook.utils.ResUtils;
 
 import org.androidannotations.annotations.AfterViews;
@@ -15,7 +16,7 @@ import org.androidannotations.annotations.ViewById;
  * Created by blanke on 16-4-25.
  */
 @EActivity(R.layout.activity_settings)
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends BaseSwipeBackActivity {
     @ViewById(R.id.activity_settings_toolbar)
     Toolbar toolbar;
 
@@ -29,4 +30,14 @@ public class SettingsActivity extends AppCompatActivity {
                 replace(R.id.activity_settings_layout, new SettingsFragment())
                 .commit();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
